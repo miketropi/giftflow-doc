2e183650c7435d97a7a230c5f7089a8ae1a40be5
Mike <mike.beplus@gmail.com>
2026-03-16 15:42:46 +0700

    First commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span title="Giftflow docs">Giftflow docs - v1.0.4</span>,
+  project: {
+    link: 'https://github.com/miketropi/giftflow',
+  },
+  chat: {
+    link: 'https://giftflow.beplus-agency.cloud/#',
+  },
+  docsRepositoryBase: 'https://giftflow.beplus-agency.cloud/#',
+  footer: {
+    text: 'Giftflow docs - v1.0.4',
+  },
+}
+
+export default config
